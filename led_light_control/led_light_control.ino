dff6cfe113c5aa80493772b3ac5694d535c41a1c
fisher60 <39353605+fisher60@users.noreply.github.com>
2019-06-17 11:36:18 -0700

    FastLed individually addressable LED code
    This is the framework to control Individually addressable LEDS using Arduino and the FastLed library.

---
+++ led_light_control/led_light_control.ino
@@ -0,0 +1,46 @@
+#include <FastLED.h>
+
+#define NUM_LEDS 9
+#define LED_PIN 2
+
+CRGB led[NUM_LEDS];
+
+void setup() {
+  
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(led, NUM_LEDS);
+
+  for (int i = 0; i < NUM_LEDS; i++){
+    led[i] = CRGB(0, 0, 255);
+    }
+  FastLED.show();
+}
+
+void setBlue(int val){
+  for (int i = 0; i < NUM_LEDS; i++){
+    led[i] = CRGB(0, 0, val);
+    }
+    FastLED.show();
+  }
+void setRed(int val){
+  for (int i = 0; i < NUM_LEDS; i++){
+    led[i] = CRGB(val, 0, 0);
+    }
+    FastLED.show();
+  }
+
+void setGreen(int val){
+  for (int i = 0; i < NUM_LEDS; i++){
+    led[i] = CRGB(0, val, 0);
+    }
+    FastLED.show();
+  }
+void loop() {
+  for (int i = 0; i < NUM_LEDS; i++){
+    led[i] = CRGB(0, 255, 0);
+    FastLED.show();
+    delay(100);
+    led[i] = CRGB(0, 0, 255);
+    FastLED.show();
+    }
+    
+  } 
